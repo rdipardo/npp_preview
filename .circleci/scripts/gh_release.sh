d53912004aca2ac462c8b623b95c57cd7b6ef0bf
Robert Di Pardo <dipardo.r@gmail.com>
2026-05-24 16:16:30 -0400

    Release 1.4.4.2

--- .circleci/scripts/gh_release.sh
+++ .circleci/scripts/gh_release.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 #
-# Copyright (c) 2022,2025 Robert Di Pardo
+# Copyright (c) 2022,2025,2026 Robert Di Pardo
 # License: https://github.com/rdipardo/nppFSIPlugin/blob/master/.circleci/scripts/gh_release.sh
 #
-test -z "$GH_API_TOKEN_2025" && exit 0
+test -z "$GH_API_TOKEN_2026" && exit 0
 
 # https://discuss.circleci.com/t/circle-branch-and-pipeline-git-branch-are-empty/44317/3
 COMMIT=$(git rev-parse "${CIRCLE_TAG:-@}") \
@@ -18,13 +18,14 @@ printf '#### SHA256 Checksums\\n\\n' > sha256sums.md
 for slug in "${ASSETS[@]}"; do printf '\\t%s\\n' "$(sha256sum "$slug")" >> sha256sums.md; done
 curl -sL -X POST \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${GH_API_TOKEN_2025}" \
+    -H "Authorization: Bearer ${GH_API_TOKEN_2026}" \
+    -H "X-GitHub-Api-Version: 2026-03-10" \
         "https://api.github.com/repos/${CIRCLE_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases" \
     -d "{\"tag_name\":\"${CIRCLE_TAG}\",
         \"target_commitish\":\"${BRANCH}\",
         \"name\":\"${TAG_NAME}\",
         \"body\":\"$(cat sha256sums.md)\",
-        \"draft\":false,
+        \"draft\":true,
         \"prerelease\":${PRE_RELEASE},
         \"generate_release_notes\":false}" > response.json
 
@@ -35,8 +36,8 @@ for slug in "${ASSETS[@]}"
 do
     curl -sL -X POST \
       -H "Accept: application/vnd.github+json" \
-      -H "Authorization: Bearer ${GH_API_TOKEN_2025}" \
-      -H "X-GitHub-Api-Version: 2022-11-28" \
+      -H "Authorization: Bearer ${GH_API_TOKEN_2026}" \
+      -H "X-GitHub-Api-Version: 2026-03-10" \
       -H "Content-Type: application/octet-stream" \
       "https://uploads.github.com/repos/${CIRCLE_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases/${RELEASE_ID}/assets?name=${slug}" \
       --data-binary "@${slug}" >/dev/null 2>&1
